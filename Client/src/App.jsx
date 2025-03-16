e396f1f577ae5e8e82fbb3810581e7ba19558aa1
Natnael47 <nati47m10@gmail.com>
2025-03-16 22:35:18 +0300

    Contact us page not complete#$

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import About from './components/About'
+import Contact from './components/Contact'
 import Header from './components/Header'
 import Projects from './components/Projects'
 import Testimonials from './components/Testimonials '
@@ -11,6 +12,7 @@ const App = () => {
       <About />
       <Projects />
       <Testimonials />
+      <Contact />
     </div>
   )
 }
