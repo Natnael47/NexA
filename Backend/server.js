d5f50bf3961ba40d43b813dec1b469e35a4825f5
Natnael47 <nati47m10@gmail.com>
2025-03-17 10:45:17 +0300

    #setup the backend

---
+++ Backend/server.js
@@ -0,0 +1,20 @@
+import cors from "cors";
+import "dotenv/config";
+import express from "express";
+
+//app config
+const app = express();
+const port = process.env.PORT || 4000;
+
+//middlewares
+app.use(express.json());
+app.use(cors());
+
+//Api endpoints
+app.get("/", (req, res) => {
+  res.send("Api Start Working");
+});
+
+app.listen(port, () => {
+  console.log(`Server Started on http://localhost:${port}`);
+});
