a23eadc2fc3d84d1cce0650d7be084080ae0f754
Natnael47 <nati47m10@gmail.com>
2025-03-18 20:03:07 +0300

    #setup folder structure

---
+++ Backend/routes/userRoute.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const userRouter = express.Router();
+
+// userRouter.post("/register", registerUser);
+// userRouter.post("/login", loginUser);
+//
+// userRouter.get("/check", auth_user, checkUserTerm);
+// userRouter.post("/update-term", auth_user, updateUserTerm);
+
+export default userRouter;
--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,13 @@
 import cors from "cors";
 import "dotenv/config";
 import express from "express";
+import { connectDB } from "./config/mongodb.js";
+import userRouter from "./routes/userRoute.js";
 
 //app config
 const app = express();
 const port = process.env.PORT || 4000;
+connectDB();
 
 //middlewares
 app.use(express.json());
@@ -12,8 +15,9 @@ app.use(cors());
 
 //Api endpoints
 app.get("/", (req, res) => {
-  res.send("Api Start Working");
+  res.send("Api Start Working!");
 });
+app.use("/api/user", userRouter);
 
 app.listen(port, () => {
   console.log(`Server Started on http://localhost:${port}`);
